17be1e73c0cb051d474855c58e89b56c3d7c7425
Felipe <felipegabrielsantosro@gmail.com>
2026-04-06 21:42:00 -0400

    Update product.js

--- app/view/pages/assets/js/product.js
+++ app/view/pages/assets/js/product.js
@@ -1,5 +1,14 @@
+import { get } from "jquery";
+
+//import SellingPriceCalculator from './components/SellingPriceCalculator.js';
 const Action = document.getElementById('action');
 const Id = document.getElementById('id');
+const totaltax = document.getElementById('total_imposto');
+const profitMargin = document.getElementById('margem_lucro');
+const operatingCost = document.getElementById('custo_operacional');
+const purchasePrice = document.getElementById('preco_compra');
+const sellingPrice = document.getElementById('preco_venda');
+const form = document.getElementById('form');
 Inputmask("currency", {
     radixPoint: ',',
     inputtype: "text",
@@ -21,7 +30,20 @@ imputMask("currency", {
     onBeforeMask: function (value) {
         return String(value).replace('.', ',');
     }
-}).mask("#preco_venda, #preco_compra");
+}).mask("#total_imposto, #margem_lucro, #custo_operacional");
+
+totaltax.addEventListener('keypress',);
+const tax = String(totaltax.value).replace( 'R$ ', '').replace('.', '').replace(',', '.');
+const result = SellingPriceCalculator.create()
+addTotalTax(tax)
+getData();
+document.getElementById('total_imposto_value').innerHTML = ${result.valor_total_imposto};
+document.getElementById('margem_lucro_value').innerHTML = ${result.valor_margem_lucro};
+document.getElementById('preco_venda_value').innerHTML = ${result.valor_venda_sugerido}             
+
+
+
+
 
 //  CARREGA DADOS DE EDIÇÃO (se existirem)
 (async () => {
